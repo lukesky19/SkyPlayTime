4fd3647c382d88d9ecbaa8a875956365f7fb12b2
lukeskywlker19 <thelukeskywlker19@gmail.com>
2026-07-08 21:11:10 -0400

    Bump SkyLib dependency to 2.0.2.0. Add 26.2 support. Update gradle wrapper to 9.6.1.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.lukesky19"
-version = "0.4.0.0"
+version = "0.4.1.0"
 
 configurations {
     testImplementation {
@@ -36,7 +36,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 
     // SkyLib
-    compileOnly("com.github.lukesky19:SkyLib:2.0.0.0")
+    compileOnly("com.github.lukesky19:SkyLib:2.0.2.0")
 
     // Integration
     compileOnly("com.github.lukesky19:NewPlayerPerks:1.4.0.0")
